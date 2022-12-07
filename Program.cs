aed4aaf4a37e51b01973670630a6bba7abe8548e
[shaigak] <shaigak@code.edu.az>
2022-12-08 00:42:27 +0300

    new task

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+//Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın
+//int a = 21;
+//if(a%3==0 & a % 7 == 0)
+//{
+//    Console.WriteLine("Eded 3 ve 7 bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Eded 3 ve 7 bolunmur");
+//}
+
+
+
+//a və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın
+//int a = 35;
+//int b = 40;
+//if (a % 2 == 0 & b % 2 == 0)
+//{
+//    Console.WriteLine(a + b);
+//}
+//else
+//{
+//    Console.Write("Duzgun eded daxil edin");
+//}
+
+
+//Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+////int a = 27;
+////int b = 40;
+////int sum = 0;
+////for (int i = 0; i <= b; i++)
+////    if (a < b && i % 2 == 1)
+////    {
+////        sum += i;
+////    }
+////Console.WriteLine(sum);
+
+
+//Verilmish arrayin icindeki tek ededlerin cemini tapin
+
+//int topla = 0;
+//int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+//foreach (var item in nums)
+//{
+//    if (item % 2 == 1)
+//        topla += item;
+//}
+//Console.WriteLine(topla);
+
+
+
+//Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+//int[] nums = { 1, 2, 3, 4, 5, 5, 7, 8, 9, 10, 11, 12 };
+//int count = 0;
+//foreach (var item in nums)
+//{
+//    if (item % 2 == 0)
+//        count++;
+//}
+//Console.WriteLine(count);
